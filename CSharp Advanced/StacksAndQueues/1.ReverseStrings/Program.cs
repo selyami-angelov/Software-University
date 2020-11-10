d0f4cb286c0637131e1cb0df9c5058b9df7f49f5
katarche <31028825+katarche@users.noreply.github.com>
2020-11-10 21:08:04 +0200

    start task

---
+++ CSharp Advanced/StacksAndQueues/1.ReverseStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
